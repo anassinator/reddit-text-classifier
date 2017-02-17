f667bdc51b3dbe6c2c1fab164b92e1208a774b28
Anass Al <mr@anassinator.com>
2017-02-16 20:14:36 -0500

    Allow sparse matrices in Poisson Naive Bayes

--- naive_bayes/naive_bayes.py
+++ naive_bayes/naive_bayes.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import Counter
 from classifier import Classifier
 from sklearn.preprocessing import label_binarize
+from sklearn.utils.extmath import safe_sparse_dot
 
 
 def preprocess_output(y):
@@ -52,7 +53,7 @@ class MultinomialNaiveBayes(Classifier):
 
         # Compute conditional probabilities.
         Y = label_binarize(y, self.classes_)
-        self.feature_count_ = Y.T * X + 1
+        self.feature_count_ = safe_sparse_dot(Y.T, X) + 1
         self.feature_log_prob_ = compute_log_prob(self.feature_count_, axis=1)
 
     def predict(self, X):
@@ -88,14 +89,14 @@ class PoissonNaiveBayes(Classifier):
         smoothed_f = self._compute_smoothed_frequency(X)
         self._tau = 1.0 / smoothed_f.min()
         smoothed_f *= self._tau
-        self.lambda_ = np.dot(Y.T, smoothed_f)
+        self.lambda_ = safe_sparse_dot(Y.T, smoothed_f)
         self.lambda_ /= self.class_count_.reshape(-1, 1)
 
         # Compute means of P(f|c').
         # Flip the one-hot encoding's bits.
         Y_not = np.ones(len(self.classes_), dtype=np.int) - Y
         class_count_not = self.class_count_.sum() - self.class_count_
-        self.lambda_not_ = np.dot(Y_not.T, smoothed_f)
+        self.lambda_not_ = safe_sparse_dot(Y_not.T, smoothed_f)
         self.lambda_not_ /= class_count_not.reshape(-1, 1)
 
     def predict(self, X):
@@ -113,13 +114,13 @@ class PoissonNaiveBayes(Classifier):
             self._log_prob(f, self.lambda_[c]).sum(axis=1) -
             self._log_prob(f, self.lambda_not_[c]).sum(axis=1)
             for c in range(n_classes)
-        ]).T
+        ]).T[0]
 
         p_c = np.exp(z + self.class_log_prior_)
         p_c_not = 1.0 - np.exp(self.class_log_prior_)
         p = p_c / (p_c + p_c_not)
 
-        return self.classes_[np.argmax(p, axis=1)]
+        return self.classes_[np.argmax(z, axis=1)]
 
     def _log_prob(self, f, mean):
         """Computes the log probability on poisson distribution.
@@ -131,7 +132,7 @@ class PoissonNaiveBayes(Classifier):
         Returns:
             Log probability.
         """
-        return f * np.log(mean) - mean
+        return safe_sparse_dot(f, np.log(mean).T) - mean
 
     def _compute_smoothed_frequency(self, X, alpha=1.0):
         """Computes the smoothed frequency.
@@ -145,5 +146,7 @@ class PoissonNaiveBayes(Classifier):
         """
         _, n_features = X.shape
         document_lengths = X.sum(axis=1).reshape(-1, 1)
-        smoothed_f = (X + alpha) / (document_lengths + alpha * n_features)
+        norm = document_lengths + alpha * n_features
+        smoothed_f = X / norm
+        smoothed_f += alpha / norm
         return smoothed_f
